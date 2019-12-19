d72514ee5ec9e00762462f694bf473b7945192e4
Marlon Paulo <marlon.paulo.silva@outlook.com>
2019-12-19 02:01:31 -0300

    finalizando edit

--- app/Views/news/form.php
+++ app/Views/news/form.php
@@ -4,14 +4,14 @@
 <form action="/news/store" method="post">
     <div class="form-group">
         <label for="title">Titulo</label>
-        <input type="text" class="form-control" name="title" id="title" value="<?php echo isset($title) ? $title : '' ?>">
+        <input type="text" class="form-control" name="title" id="title" value="<?php echo isset($title) ? $title : set_value('title') ?>">
     </div>
     <div class="form-group">
         <label for="body">Notícia</label>
-        <textarea name="body" id="body" cols="30" rows="10" class="form-control"><?php echo isset($body) ? $body : '' ?></textarea>
+        <textarea name="body" id="body" cols="30" rows="10" class="form-control"><?php echo isset($body) ? $body : set_value('body') ?></textarea>
     </div>
 
-    <input type="hidden" name="id" value="<?php echo isset($id) ? $id : '' ?>">
+    <input type="hidden" name="id" value="<?php echo isset($id) ? $id : set_value('id') ?>">
     <div class="form-group">
         <input type="submit" value="Salvar" class="btn btn-primary">
     </div>
